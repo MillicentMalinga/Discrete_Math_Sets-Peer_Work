33e47242ded671c7d6d317bd34caffadec50bbab
SOnatti <s.moses@alustudent.com>
2021-11-08 21:23:05 +0200

    Boolean

--- Question_1.py
+++ Question_1.py
@@ -1,6 +1,10 @@
+#initialize list and set
 my_list = ['Alex', 'Bob', 'Sam', 'Brian', 'Brian', 'Sam', 'Sam']
 my_set =set(my_list)
 
+#function to test
+'''If statement checks whether list elements constitutes set. Outputs True when the given list elements constitues the set,
+and false when they do not. When True no other statement displayed. When false, False is displayed plus the true set'''
 def check(lst):
     if lst in my_set:
         return True
