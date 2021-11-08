9bf1d0b0e11c51ad734e7b4b1246200ff9e730a1
Ikudehinbu Beauty <86835129+mzbhewtee@users.noreply.github.com>
2021-11-08 21:35:07 +0200

    Update Question_5.py

--- Question_5.py
+++ Question_5.py
@@ -1,5 +1,5 @@
 #lines 2 to 7 ask the user for input for the lists
-x0 = int(input("Enter the first for the first list\n"))
+x0 = int(input("Enter the first number for the first list\n"))
 x1 = int(input("Enter the second number for the first list\n"))
 x2 = int(input("Enter the third number for the first list\n"))
 y0 = int(input("Enter the first for the second list\n"))
@@ -18,6 +18,7 @@ def truth_false(a, b):
         for p in b:
             #appending the modulus to the dictionary
             collective.append(p%i)
+    #printing the collective for verification
     print(collective)
     #checking if any element in the dictionary is equal to zero
     if any(collective) == 0:
